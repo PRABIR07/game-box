373fea563b73e27474e9febae63b1dd3e48724d0
PRABIR07 <uic.19mca1007@gmail.com>
2023-06-21 09:35:18 +0530

    Get optimized Images

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -33,11 +33,11 @@ const PlatformIconList = ({ platforms }: Props) => {
 
     }
   return (
-    <HStack marginY={'10px'}>
+    <HStack marginY={"10px"}>
       {platforms.map((platform) => (
-       <Icon as={iconMap[platform.slug]}/>
+        <Icon key={platform.id} as={iconMap[platform.slug]} />
       ))}
-   </HStack>
+    </HStack>
   );
 };
 
