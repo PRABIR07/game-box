7f3e118247e40c9d931b115c821ef1382899e810
PRABIR07 <uic.19mca1007@gmail.com>
2023-06-30 14:18:14 +0530

    Feat: Implement Searchbar functionality

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,12 +2,15 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+interface Props {
+  onSearch: (searchText: string) => void;
+}
 
-const NavBar = () => {
+const NavBar = ({onSearch}:Props) => {
   return (
     <HStack padding="10px">
       <Image src={logo} boxSize="60px" />
-      <SearchInput/>
+      <SearchInput onSearch={onSearch}/>
       <ColorModeSwitch />
     </HStack>
   );
