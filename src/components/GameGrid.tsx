70702edad1375e50809ff0a74e9efe8e1701c044
PRABIR07 <uic.19mca1007@gmail.com>
2023-06-30 18:18:25 +0530

    Title name changed

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,9 +14,9 @@ interface Props {
 const GameGrid = ({gameQuery}: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const Skeletons = [1, 2, 3, 4, 5, 6,7,8,9,10,11,12];
+  if(error) return <Text>{error}</Text>
   return (
-    <>
-      {error && <Text>{error}</Text>}
+   
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding='10px'
@@ -34,7 +34,7 @@ const GameGrid = ({gameQuery}: Props) => {
           </GameCardContainer>
         ))}
       </SimpleGrid>
-    </>
+    
   );
 };
 
